4d66975572705419649bfff939e9423dfe4e6f28
Gueho Frederic <32833257+FredGho@users.noreply.github.com>
2020-11-25 12:14:12 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="FGO TestApp2 CleverCloud"/>
   </div>
 </template>
 
