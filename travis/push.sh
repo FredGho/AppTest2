3bd1c28533b29c60cfb46be0a3e0e69d47f2cd4c
FredGho <frederic.gueho@imie.fr>
2020-11-25 16:02:59 +0100

    modif4

--- travis/push.sh
+++ travis/push.sh
@@ -1,1 +1,1 @@
-https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_ee5dcd49-99c0-4378-a0d8-a4de8a6bb109.git --verbose --force push origin master
+git remote add apptest2 https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_ee5dcd49-99c0-4378-a0d8-a4de8a6bb109.git --verbose --force push origin master
